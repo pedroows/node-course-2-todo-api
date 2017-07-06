5497236f9fd601b1cf9afa222daece76d447fbeb
PedroBrito <p.1392@hotmail.com>
2017-07-06 09:48:31 -0300

    Fixed db path

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect("mongodb://admin:admin@ds028310.mlab.com:28310/node-todo-api");
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect("mongodb://admin:admin@ds028310.mlab.com:28310/node-todo-api");
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
