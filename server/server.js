4e1e22945af34a87712cdc52420a73479d5652b0
PedroBrito <p.1392@hotmail.com>
2017-07-06 16:48:32 -0300

    Add PATCH  /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -66,8 +68,42 @@ app.delete('/todos/:id', (req, res)=>{
   });
 });
 
+app.patch('/todos/:id', (req, res)=>{
+  console.log('Commando received');
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  console.log(body);
+  console.log(body.text);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Id not valid");
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+});
+
+
 app.listen(port, ()=>{
   console.log(`Started on port ${port}`);
 });
+
+
 
 module.exports={app};
