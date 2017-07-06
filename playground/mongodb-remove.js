55ab2f9b963c67f2420b2c27de247080cca25590
PedroBrito <p.1392@hotmail.com>
2017-07-06 09:46:09 -0300

    Add DELETE /todos/:id route

---
+++ playground/mongodb-remove.js
@@ -0,0 +1,10 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+//Todo.remove({}).then((result)=>{
+//  console.log(result);
+//});
+
+//Todo.findOneAndRemove
+//Todo.findByIdAndRemove
--- server/server.js
+++ server/server.js
@@ -33,22 +33,35 @@ app.get('/todos', (req, res)=>{
 
 app.get('/todos/:id', (req, res)=>{
   var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Id not valid");
+  }
+
   Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(400).send('Id not found');
+    }
+    res.send({todo});
+  }, (e)=>{
+    res.status(400).send(e);
+  });
+});
 
-    if(ObjectID.isValid(id)){
-      if(!todo){
-        console.log(ObjectID.isValid(id));
-        return res.status(400).send('Id not found');
-      }
+app.delete('/todos/:id', (req, res)=>{
+  var id = req.params.id;
 
-      res.send({todo});
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send("Id not valid");
+  }
 
-    }else {
-      return res.status(404).send('Id is invalid');
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
     }
 
-
-  }, (e)=>{
+    res.send(todo);
+  }).catch((e)=>{
     res.status(400).send(e);
   });
 });
